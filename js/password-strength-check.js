b28e42910f7e7538c974cc7edb631037a839eb5b
Omar Shabaan <omar.plus.077@gmail.com>
2022-05-10 02:43:04 +0200

    all

---
+++ js/password-strength-check.js
@@ -0,0 +1,55 @@
+const password = document.getElementById("PassEntry");
+const confirm = document.getElementById("repeatPassEntry");
+const strengthBadge = document.getElementById("StrengthDisp");
+console.log(password);
+console.log(strengthBadge);
+
+
+const strongPassword = new RegExp(
+  "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+);
+
+const mediumPassword = new RegExp(
+  "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
+);
+
+function StrengthChecker(pass) {
+  if (strongPassword.test(pass)) {
+    strengthBadge.style.backgroundColor = "green";
+    strengthBadge.textContent = "Strong";
+  } else if (mediumPassword.test(pass)) {
+    strengthBadge.style.backgroundColor = "blue";
+    strengthBadge.textContent = "Medium";
+  } else {
+    strengthBadge.style.backgroundColor = "red";
+    strengthBadge.textContent = "Weak";
+  }
+}
+
+// Adding an input event listener when a user types to the  password input
+password.addEventListener("input", () => {
+
+  strengthBadge.style.display = "block";
+
+  //We then call the StrengChecker function as a callback then pass the typed password to it
+  StrengthChecker(password.value);
+
+  //Incase a user clears the text, the badge is hidden again
+
+  if (password.value.length !== 0) {
+    strengthBadge.style.display = "block";
+  } else {
+    strengthBadge.style.display = "none";
+  }
+});
+
+// Adding an input event listener when a user confirm the  password input
+function confirmPass() {
+  if (confirm.value === password.value) {
+    document.getElementById("confirmPass").innerHTML = "";
+  } else {
+    document.getElementById("confirmPass").innerHTML = "Passwords do not match";
+  }
+}
+
+confirm.addEventListener("input", confirmPass);
---
+++ js/script.js
@@ -0,0 +1,73 @@
+const pageOne = document.getElementById('page-1')
+const pageTwo = document.getElementById('page-2')
+
+const pageView = document.getElementById('page-view')
+console.log("============")
+console.log(pageView)
+console.log(pageOne)
+console.log(pageTwo)
+
+
+const pageOneContent = `<div class="col-lg-4 col-md-6 py-3 px-5">
+<div class="card w-100 h-100">
+    <img src="img/latte.jpg" class="card-img-top img-fluid h-100" alt="latte">
+    <div class="card-body px-5">
+        <h5 class="card-title">Latte</h5>
+        <p class="card-text">Cappuccino with a double portion of milk and a more liquid foam.</p>
+    </div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 py-3 px-5">
+<div class="card w-100 h-100">
+    <img src="img/cappuccino.jpg" class="card-img-top img-fluid h-100" alt="cappuccino">
+    <div class="card-body px-5">
+        <h5 class="card-title">Cappuccino</h5>
+        <p class="card-text">This is espresso with whipped foam. Drink it without sugar, savoring the natural milky sweetness.</p>
+    </div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 py-3 px-5">
+<div class="card w-100 h-100">
+    <img src="img/pumpkin-raf.jpg" class="card-img-top img-fluid h-100" alt="pumpkin raf">
+    <div class="card-body px-5">
+        <h5 class="card-title">Pumpkin raf</h5>
+        <p class="card-text">A drink with whipped cream foam and vanilla sugar and pumpkin syrup in the base.</p>
+    </div>
+</div>
+</div>`
+
+const pageTwoContent = `<div class="col-lg-4 col-md-6 py-3 px-5">
+<div class="card w-100 h-100">
+    <img src="img/Turkish-coffee.jpg" class="card-img-top img-fluid h-100" alt="turkish coffee">
+    <div class="card-body px-5">
+        <h5 class="card-title">Turkish coffee</h5>
+        <p class="card-text">Сoffee prepared by boiling ground coffee beans in a turk, sometimes with added sugar.</p>
+    </div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 py-3 px-5">
+<div class="card w-100 h-100">
+    <img src="img/expresso.jpg" class="card-img-top img-fluid h-100" alt="espresso">
+    <div class="card-body px-5">
+        <h5 class="card-title">Espresso</h5>
+        <p class="card-text">A drink for which hot water under high pressure is passed through a filter with ground coffee.</p>
+    </div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 py-3 px-5">
+<div class="card w-100 h-100">
+    <img src="img/iced-coffe.jpg" class="card-img-top img-fluid h-100" alt="ice coffee">
+    <div class="card-body px-5">
+        <h5 class="card-title">Ice coffee</h5>
+        <p class="card-text">Ice coffee a cold drink based on coffee with ice cream. </p>
+    </div>
+</div>
+</div>`
+
+pageOne.addEventListener('click', () => {
+    pageView.innerHTML = pageOneContent
+})
+
+pageTwo.addEventListener('click', () => {
+    pageView.innerHTML = pageTwoContent
+})
